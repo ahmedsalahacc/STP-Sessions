12c8928fb192fefaf2c5f5d4be0e260a946b7bfe
ahmedsalahacc <ahmedsalaheldin125@gmail.com>
2021-10-04 10:09:50 +0200

    configured frontend files and added guiding comments

--- apis-session-practical/src/controllers.py
+++ apis-session-practical/src/controllers.py
@@ -1,51 +1,68 @@
 from flask import Blueprint, render_template, url_for, redirect, request
 from src.models import Todo
 
-import os
-
-
+#-- init --#
 main = Blueprint('main', __name__, )
-# print('template folder', main.template_folder)
+
+#-- endpoints --#
 
 # @TODO make an endpoint to display all todos
 @main.route('/', methods=['GET'])
 def getIndex():
-    # extract the task from parameters
-    tasks = Todo.getAll()
+    try:
+        # retrieve all items from database
+        tasks = Todo.getAll()
+    except:
+        return 500
 
     return render_template('index.html', tasks=tasks)
 
 # @TODO make save a todo to database
 @main.route('/', methods=['POST'])
 def postIndex():
-    task = request.form.get('task')
-    task = task.lstrip()
-    print(task)
-    Todo.insert(task)
+    try:
+        # get the task from request form dict
+        task = request.form.get('task')
+        # insert item to database
+        Todo.insert(task)
+    except:
+        return 500
 
     return redirect('/')
 
-
+# @TODO make endpoint to get single item
 @main.route('/edit/<id>', methods=['GET'])
 def getEdit(id):
-    task = Todo.get(id)
+    try:
+        # get item from database by id
+        task = Todo.get(id)
+    except:
+        return 500
 
     return render_template('edit.html', task=task)
 
 
+# @TODO make an endpoint edit todo
 @main.route('/edit/<id>', methods=['POST'])
 def putEdit(id):
-    task = request.form.get('task').rstrip().lstrip()
-
-    Todo.update(id, task)
+    try:
+        # get the new task from request form dict
+        task = request.form.get('task')
+        # update database of specific record
+        Todo.update(id, task)
+    except:
+        return 500
 
     return redirect('/')
-# @TODO make an endpoint edit todo
 
 
+# @TODO make an endpoint to delete todo
 @main.route('/delete/<id>', methods=['GET'])
 def deleteTask(id):
-    Todo.delete(id)
+    try:
+        # delete specific record by id
+        Todo.delete(id)
+    except:
+        return 500
 
     return redirect('/')
-# @TODO make an endpoint to delete todo
--- apis-session-practical/src/models.py
+++ apis-session-practical/src/models.py
@@ -18,32 +18,79 @@ class Todo(db.Model):
 
     @classmethod
     def insert(cls, task):
-        todo = Todo(task=task)
-        db.session.add(todo)
-        db.session.commit()
-        print("successfully inserted")
+        '''
+        class method that inserts task to database
+        ----------
+        args:
+            task - string
+        '''
+        try:
+            todo = Todo(task=task)
+            db.session.add(todo)
+            db.session.commit()
+            print("successfully inserted item into database")
+        except:
+            print("Error occured while inserting to database")
 
     @classmethod
     def update(cls, id, new_task):
-        query = cls.query.filter_by(id=id).first()
-        query.task = new_task
-        query.dateCreated = datetime.datetime.utcnow()
-        db.session.commit()
-        print(f"Successfully updated {cls}")
+        '''
+        class method that updates a specific record in the database
+        ----------
+        args:
+            id - int
+            task - string
+        '''
+        try:
+            query = cls.query.filter_by(id=id).first()
+            query.task = new_task
+            query.dateCreated = datetime.datetime.utcnow()
+            db.session.commit()
+            print(f"Successfully updated database")
+        except:
+            print("Error occured while updating database")
 
     @classmethod
     def delete(cls, id):
-        query = cls.query.filter_by(id=id).first()
-        db.session.delete(query)
-        db.session.commit()
-        print(f"Successfully deleted")
+        '''
+        class method that deletes a specific record in the database
+        ----------
+        args:
+            id - int
+        '''
+        try:
+            query = cls.query.filter_by(id=id).first()
+            db.session.delete(query)
+            db.session.commit()
+            print(f"Successfully deleted item from database")
+        except:
+            print("Error occured while deleting from database")
 
     @classmethod
     def get(cls, id):
-        todo = cls.query.filter_by(id=id).first()
+        '''
+        class method that gets a specific record from database
+        ----------
+        args:
+            id - int
+        '''
+        try:
+            todo = cls.query.filter_by(id=id).first()
+            print("Success retreiving data from database")
+        except:
+            print("Error occured while retrieving from database")
         return todo
 
     @classmethod
     def getAll(cls):
-        todos = cls.query.order_by(cls.dateCreated.desc()).all()
+        '''
+        class method that gets all records in database and sort them
+        by date in descending order
+        ----------
+        '''
+        try:
+            todos = cls.query.order_by(cls.dateCreated.desc()).all()
+            print("Success retreiving data from database")
+        except:
+            print("Error occured while retrieving from database")
         return todos
